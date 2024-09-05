19a1ed87bd35bbf83fad1f9b478c026723ba3abb
Jose Alonso <josemalonsom@yahoo.es>
2024-09-05 21:30:44 +0200

    Reorganise and add tests for 2015 day 02

---
+++ python/tests/year2015/test_day02.py
@@ -0,0 +1,23 @@
+import pytest
+
+from tests.helper import get_file
+from year2015.day02 import solve
+
+class TestDay02:
+
+    @pytest.mark.parametrize("puzzle_input, expected_wrapping_paper, expected_ribbon", [
+        (["2x3x4"], 58, 34),
+        (["1x1x10"], 43, 14)
+    ])
+    def test_wrapping_paper_examples(self, puzzle_input, expected_wrapping_paper, expected_ribbon):
+        result = solve(puzzle_input)
+
+        assert result["total_square_feet_of_wrapping_paper"] == expected_wrapping_paper
+        assert result["total_ribbon_length"] == expected_ribbon
+
+    def test_puzzle(self):
+        puzzle_input = get_file("data.year2015", "day-02-input.txt").readlines()
+        result = solve(puzzle_input)
+
+        assert result["total_square_feet_of_wrapping_paper"] == 1606483
+        assert result["total_ribbon_length"] == 3842356
